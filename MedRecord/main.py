ba9a461ca1480bb9df5338f6e26db6fee23eede3
med260 <medos80268026@gmail.com>
2025-09-05 23:20:59 +0300

    Public Repositry

---
+++ MedRecord/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from routes import auth
+from routes import users, patients, doctors, ailments, prescriptions
+
+# Create the app
+app = FastAPI(title="Medical Record API", version="1.0")
+
+app.include_router(auth.router)
+app.include_router(patients.router)
+app.include_router(doctors.router)
+app.include_router(ailments.router)
+app.include_router(prescriptions.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "Welcome to the Medical Record API!"}
---
+++ MedRecord/routes/ailments.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+import crud, schemas
+from DB.database import get_db
+
+router = APIRouter(prefix="/ailments", tags=["Ailments"])
+
+
+@router.post("/patient/{patient_id}", response_model=schemas.AilmentResponse)
+def create_ailment(patient_id: int, ailment: schemas.AilmentCreate, db: Session = Depends(get_db)):
+    return crud.create_ailment(db, ailment, patient_id)
+
+
+@router.get("/", response_model=list[schemas.AilmentResponse])
+def read_ailments(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_ailments(db, skip=skip, limit=limit)
+
+
+@router.get("/patient/{patient_id}", response_model=list[schemas.AilmentResponse])
+def get_patient_ailments(patient_id: int, db: Session = Depends(get_db)):
+    return crud.get_patient_ailments(db, patient_id)
+
+
+@router.delete("/{ailment_id}", response_model=schemas.AilmentResponse)
+def delete_ailment(ailment_id: int, db: Session = Depends(get_db)):
+    ailment = crud.delete_ailment(db, ailment_id)
+    if not ailment:
+        raise HTTPException(status_code=404, detail="Ailment not found")
+    return ailment
